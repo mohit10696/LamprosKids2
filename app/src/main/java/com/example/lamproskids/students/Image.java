74372146ef91364d0f38ab3ef970e3121566f027
mohit <mohitsojitraa@gmail.com>
2020-01-06 17:14:36 +0530

    image update

---
+++ app/src/main/java/com/example/lamproskids/students/Image.java
@@ -0,0 +1,77 @@
+package com.example.lamproskids.students;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.lamproskids.R;
+import java.util.ArrayList;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+class model_image {
+    public model_image() {
+    }
+    public model_image(int image, String name) {
+        this.image = image;
+        this.name = name;
+    }
+
+    public int image;
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String name;
+
+    }
+class holder_image extends RecyclerView.ViewHolder{
+        TextView text1;
+        ImageView imageView;
+        public holder_image(@NonNull View itemView) {
+            super(itemView);
+            this.text1 = itemView.findViewById(R.id.imagename);
+            this.imageView = itemView.findViewById(R.id.imageview);
+        }
+}
+class adpater_image extends RecyclerView.Adapter<holder_image>{
+    Context c;
+    ArrayList<model_image> model;
+    public adpater_image(Context c, ArrayList<model_image> model) {
+        this.c = c;
+        this.model = model;
+    }
+
+    @NonNull
+    @Override
+    public holder_image onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_image,null);
+        return new holder_image(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull holder_image holder, int position) {
+        holder.text1.setText(model.get(position).getName());
+        holder.imageView.setImageResource(model.get(position).getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return model.size();
+    }
+}
